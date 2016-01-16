c2c71e393538e56f92ef61e744a601a1f53b3f71
binderix <bwaedt@yahoo.de>
2016-01-16 23:29:13 +0100

    Star commit

---
+++ MvcMusikStore/Controllers/StoreController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+
+namespace MvcMusicStore.Content
+{
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            //SampleData sd = new SampleData();
+            //sd.InitializeDatabase(storeDB);
+            //storeDB.SaveChanges();
+
+
+            var genres = storeDB.Genres.ToList();
+           
+
+            //var genres = new List<Genre>
+            //{
+            //    new Genre { Name = "Disco" },
+            //    new Genre { Name = "Playing for Change"},
+            //    new Genre { Name = "native american shamanic music"},
+            //    new Genre { Name = "Forroh"}
+            //};
+
+            return View(genres);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+
+            return View(genreModel);
+        }
+
+    }
+}
